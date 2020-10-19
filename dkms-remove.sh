022742ac6c4058a4d54e690cc3b837e0b2a41000
morrownr <morrownr@gmail.com>
2020-10-19 11:26:00 -0500

    fix logic error in remove script

--- dkms-remove.sh
+++ dkms-remove.sh
@@ -9,14 +9,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-echo ""
-echo "Removing /etc/modprobe.d/88x2bu.conf"
-rm -f /etc/modprobe.d/88x2bu.conf
-
-echo ""
-echo "Removing /usr/src/${DRV_NAME}-${DRV_VERSION)"
-rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-
 dkms remove ${DRV_NAME}/${DRV_VERSION} --all
 RESULT=$?
 
@@ -24,6 +16,11 @@ if [[ "$RESULT" != "0" ]]; then
   echo "An error occurred while running dkms remove"
   exit $RESULT
 else
-  echo "dkms-remove.sh was successful."
+	echo ""
+	echo "Removing /etc/modprobe.d/88x2bu.conf"
+	rm -f /etc/modprobe.d/88x2bu.conf
+	echo ""
+	echo "Removing /usr/src/${DRV_NAME}-${DRV_VERSION)"
+	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
+	echo "dkms-remove.sh was successful."
 fi
-
