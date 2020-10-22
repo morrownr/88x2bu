d80d995dda942c7df1c041e79e921c639a018e76
morrownr <morrownr@gmail.com>
2020-10-22 00:34:09 -0500

    various updates

--- dkms-remove.sh
+++ dkms-remove.sh
@@ -16,11 +16,7 @@ if [[ "$RESULT" != "0" ]]; then
   echo "An error occurred while running dkms remove"
   exit $RESULT
 else
-	echo ""
-	echo "Removing /etc/modprobe.d/88x2bu.conf"
 	rm -f /etc/modprobe.d/88x2bu.conf
-	echo ""
-	echo "Removing /usr/src/${DRV_NAME}-${DRV_VERSION)"
 	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
 	echo "dkms-remove.sh was successful."
 fi
