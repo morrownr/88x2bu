4cf1ffdcbf1e15721bfeafb33a173e49e4a0dc73
morrownr <morrownr@gmail.com>
2020-12-22 22:06:19 -0600

    no retries when retry=false

--- hal/rtl8822b/usb/rtl8822bu_xmit.c
+++ hal/rtl8822b/usb/rtl8822bu_xmit.c
@@ -259,7 +259,7 @@ static s32 update_txdesc(struct xmit_frame *pxmitframe, u8 *pmem, s32 sz, u8 bag
 		if (pattrib->retry_ctrl == _TRUE)
 			SET_TX_DESC_RTS_DATA_RTY_LMT_8822B(ptxdesc, 6);
 		else
-			SET_TX_DESC_RTS_DATA_RTY_LMT_8822B(ptxdesc, 12);
+			SET_TX_DESC_RTS_DATA_RTY_LMT_8822B(ptxdesc, 0);
 
 		/* VHT NDPA or HT NDPA Packet for Beamformer. */
 		rtl8822b_fill_txdesc_mgnt_bf(pxmitframe, ptxdesc);
